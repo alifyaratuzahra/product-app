5e0e58aa28c6716e68d6828a741a6aaf1c69966a
divisidata <divisidatalitbang@gmail.com>
2025-02-22 09:58:23 +0700

    project-updated6

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'My Project.';
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'Project menggunakan Next.js and MongoDB';
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
